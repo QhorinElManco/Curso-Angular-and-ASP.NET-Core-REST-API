6cb8f8fd810eb5dad4c437f33441fd6b174f4a2f
Maynor Pineda <sbethuell@gmail.com>
2025-04-23 22:49:38 -0600

    Agrega elementos iniciales de interfaz

---
+++ CodePulse.UI/src/app/features/category/add-category/add-category.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-category',
+  imports: [],
+  templateUrl: './add-category.component.html',
+  styleUrl: './add-category.component.css'
+})
+export class AddCategoryComponent {
+
+}
+

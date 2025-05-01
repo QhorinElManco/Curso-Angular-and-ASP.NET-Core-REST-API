c648bf3af304c485ae3f6933b3f0e6aad16b3b24
Maynor Pineda <sbethuell@gmail.com>
2025-05-01 11:54:32 -0600

    Add live Markdown preview and image preview in blog form
    Integrate `ngx-markdown` and `prismjs` for live Markdown preview in the blog post form. Adjust the form layout to display a side-by-side editor and preview section, and add a real-time image preview for the featured image URL. Update dependencies to the latest versions to support these features.

--- CodePulse.UI/src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
+++ CodePulse.UI/src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -1,16 +1,20 @@
 import {Component, OnDestroy} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {AddBlogRequestModel} from '../models/add-blog-request.model';
-import {DatePipe} from '@angular/common';
+import {CommonModule, DatePipe, NgOptimizedImage} from '@angular/common';
 import {Subscription} from 'rxjs';
 import {BlogpostService} from '../services/blogpost.service';
 import {Router} from '@angular/router';
+import {MarkdownComponent} from 'ngx-markdown';
 
 @Component({
   selector: 'app-add-blogpost',
   imports: [
     FormsModule,
-    DatePipe
+    DatePipe,
+    MarkdownComponent,
+    CommonModule,
+    NgOptimizedImage
   ],
   templateUrl: './add-blogpost.component.html',
   styleUrl: './add-blogpost.component.css'
